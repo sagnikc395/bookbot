d382312414fdc4fa03ef720ed0f5b5330387ebd1
sagnikc395 <sagnikchatterjee395@gmail.com>
2024-03-29 09:34:43 +0530

    src: count words

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
+def count_words(content):
+    #counts the number of words in the string 
+    return len(content.split(' '))
+
+
 def main():
     with open("books/frankenstein.txt") as f:
         contents = f.read()
-        print(contents)
+        #print(contents)
+    
+    print(f"Word Count: {count_words(contents)}")
 
 
 main()
