7978c9fe600fb15490bfd24751ff23333e5189aa
sagnikc395 <sagnikchatterjee395@gmail.com>
2024-03-29 17:47:19 +0530

    src: added print repor

--- main.py
+++ main.py
@@ -18,13 +18,34 @@ def count_letters(content):
     return char_dict
 
 
+def print_report(content, path_name):
+    # aggreagete all the words and character data into a nice report
+    # that we can print to the console
+    print(f"--- Begin report of {path_name} ---")
+    print(f"{count_words(content)} words found in the document\n")
+    res = dict()
+    for char in content.lower():
+        if char.isalpha():
+            if char in res:
+                res[char] += 1
+            else:
+                res[char] = 1
+    ## sorting dicts by value
+    res_sort = dict(sorted(res.items(), key=lambda item: item[1], reverse=True))
+    for k, v in res_sort.items():
+        print(f"The {k} character was found {v} times")
+    print("--- End report ---")
+
+
 def main():
     with open("./books/frankenstein.txt") as f:
         contents = f.read()
         # print(contents)
 
-    print(f"Word Count: {count_words(contents)}")
-    print(f"{count_letters(contents)}")
+    # print(f"Word Count: {count_words(contents)}")
+    # print(f"{count_letters(contents)}")
+    path_name = "/books/frankestein.txt"
+    print_report(contents, path_name)
 
 
 main()
