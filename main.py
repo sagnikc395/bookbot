a5949ac1986850062af39a4fdc093b298f160ca4
sagnikc395 <sagnikchatterjee395@gmail.com>
2025-01-03 17:54:32 +0530

    challenge completed

--- main.py
+++ main.py
@@ -1,51 +1,43 @@
-def count_words(content):
-    # counts the number of words in the string
-    return len(content.split())
+import os
 
 
-def count_letters(content):
-    # takes the text from the book as a string and returns the number of times each character
-    # appears in the string
-    # case independent
-    content = content.lower()
-    char_dict = dict()
-    for c in content:
-        if c in char_dict:
-            char_dict[c] += 1
-        else:
-            char_dict[c] = 1
-
-    return char_dict
+def word_count(text: str) -> int:
+    """
+    takes the text from the book as a string and returns
+    the number of words in the string
+    """
+    words = text.split()
+    return len(words)
 
 
-def print_report(content, path_name):
-    # aggreagete all the words and character data into a nice report
-    # that we can print to the console
-    print(f"--- Begin report of {path_name} ---")
-    print(f"{count_words(content)} words found in the document\n")
-    res = dict()
-    for char in content.lower():
-        if char.isalpha():
-            if char in res:
-                res[char] += 1
-            else:
-                res[char] = 1
-    ## sorting dicts by value
-    res_sort = dict(sorted(res.items(), key=lambda item: item[1], reverse=True))
-    for k, v in res_sort.items():
-        print(f"The {k} character was found {v} times")
-    print("--- End report ---")
+def character_count(text: str) -> dict:
+    """
+    takes the text from the book as a string, and returns the number
+    of times each character appears in the string
+    """
+    char_count = {}
+    text = text.lower()
+    for char in text:
+        if char not in char_count and char.islower():
+            char_count[char] = 1
+        elif char.islower():
+            char_count[char] += 1
+    return char_count
 
 
-def main():
-    with open("./books/frankenstein.txt") as f:
-        contents = f.read()
-        # print(contents)
-
-    # print(f"Word Count: {count_words(contents)}")
-    # print(f"{count_letters(contents)}")
-    path_name = "/books/frankestein.txt"
-    print_report(contents, path_name)
+def print_report(file_path, word_count: int, char_count: dict) -> None:
+    print(f"--- Begin report of {file_path} ---")
+    print(f"{word_count} words found in the document")
+    for char in char_count:
+        print(f"The '{char}' character was found {char_count[char]} times")
 
 
-main()
+with open("./books/frankenstein.txt") as f:
+    file_contents = f.read()
+# print(file_contents)
+
+file_path = os.path.basename("./books/frankenstein.txt")
+print(word_count(file_contents))
+print(character_count(file_contents))
+
+print_report(file_path, word_count(file_contents), character_count(file_contents))
